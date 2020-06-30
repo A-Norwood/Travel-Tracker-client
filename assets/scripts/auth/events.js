ce11be337585e872b3ba6c76cf9e59ac8d9a8d34
Ashley Norwood <norwood54@icloud.com>
2020-06-30 11:35:31 -0400

    Did not add on last commit, adding now.

---
+++ assets/scripts/auth/events.js
@@ -0,0 +1,52 @@
+const api = require('./api')
+const ui = require('./ui')
+const getFormFields = require('../../../lib/get-form-fields.js')
+
+const onSignUp = function (event) {
+  event.preventDefault()
+
+  const form = event.target
+  const data = getFormFields(form)
+
+  api.signUp(data)
+
+    .then(ui.signUpSuccess)
+    .catch(ui.signUpFailure)
+}
+
+const onSignIn = function (event) {
+  event.preventDefault()
+
+  const form = event.target
+  const data = getFormFields(form)
+
+  api.signIn(data)
+
+    .then(ui.signInSuccess)
+    .catch(ui.signInFailure)
+}
+
+const onChangePw = function (event) {
+  event.preventDefault()
+
+  const form = event.target
+  const data = getFormFields(form)
+
+  api.changePw(data)
+    .then(ui.changePwSuccess)
+    .catch(ui.changePwFailure)
+}
+
+const onSignOut = function (event) {
+  event.preventDefault()
+
+  api.signOut()
+    .then(ui.signOutSuccess)
+    .catch(ui.signOutFailure)
+}
+module.exports = {
+  onSignUp,
+  onSignIn,
+  onChangePw,
+  onSignOut
+}
---
+++ assets/scripts/auth/ui.js
@@ -0,0 +1,72 @@
+const store = require('./../store')
+
+const signUpSuccess = function (response) {
+  console.log(response)
+  $('form').trigger('reset')
+  $('#sign-up-content').text('Sign up complete ' + response.user.email)
+  $('#sign-up-content').show()
+  $('#signout-message').text('Signed out successfully. Please sign in again.').hide()
+}
+
+const signUpFailure = function () {
+  $('form').trigger('reset')
+  $('#sign-up-content').text('Sign up failed')
+  $('#sign-up-content').show()
+}
+
+const signInSuccess = function (response) {
+  $('form').trigger('reset')
+  $('#sign-up-content').text('Signed in ' + response.user.email)
+  $('#sign-up-content').show()
+  $('.hidden').removeClass()
+  $('.show').hide()
+  $('#sign-out-stuff').show()
+  $('#sign-out').show()
+  $('#change-pw').show()
+  $('#signout-message').text('Signed out successfully. Please sign in again.').hide()
+  store.user = response.user
+}
+
+const signInFailure = function (response) {
+  $('form').trigger('reset')
+  $('#sign-up-content').text('Signed in failed')
+  $('#sign-up-content').show()
+}
+
+const changePwSuccess = function () {
+  $('form').trigger('reset')
+  $('.change-pw-message').text('Password changed successfully')
+}
+
+const changePwFailure = function () {
+  $('form').trigger('reset')
+  $('.change-pw-message').text('Password change failed')
+}
+
+const signOutSuccess = function () {
+  $('#signout-message').text('Signed out successfully. Please sign in again.').show()
+  $('#new-game').addClass('hidden')
+  $('#change-pw').hide()
+  $('.show').show()
+  $('#sign-up-content').hide()
+  $('.message').hide()
+  $('#sign-out').hide()
+  $('.change-pw-message').hide()
+  console.log(store)
+}
+
+const signOutFailure = function () {
+  $('#sign-out').text('Signed Out Failed')
+  $('#sign-out').removeClass()
+}
+
+module.exports = {
+  signUpSuccess,
+  signUpFailure,
+  signInSuccess,
+  signInFailure,
+  changePwSuccess,
+  changePwFailure,
+  signOutSuccess,
+  signOutFailure
+}
