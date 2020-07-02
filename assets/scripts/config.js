3601641fdd6c312c0708030f4b7821fae0d807e9
Ashley Norwood <norwood54@icloud.com>
2020-07-02 15:31:17 -0400

    For deployment

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: 'https://salty-eyrie-87917.herokuapp.com/',
+  production: 'https://salty-eyrie-87917.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
